e7ca07c7658df2d82fc534f31d15181c312e2a42
NagamaniMannempalli <nagamanimannempalli12@gmail.com>
2026-02-11 22:50:51 +0530

    Updated frontend

--- job-application-frontend/src/api/application.js
+++ job-application-frontend/src/api/application.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080";
+const API = "https://applyboard-job-application-tracer.onrender.com";
 
 const auth = () => ({
   headers: {
--- job-application-frontend/src/api/auth.js
+++ job-application-frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/user";
+const API_URL = "https://applyboard-job-application-tracer.onrender.com/user";
 
 export const loginUser = (data) => {
   return axios.post(`${API_URL}/login`, data);
